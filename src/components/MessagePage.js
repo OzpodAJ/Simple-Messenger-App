92ab8f3c8b47865fcb4e98738d4a8513dc9269da
Andrew Osborn <osbornandrewj@gmail.com>
2022-10-28 11:15:10 -0700

    card display attempt 1

--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -1,28 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MessageForm from "./MessageForm"
 import MessageCard from "./MessageCard";
 
-function MessagePage({username, userColor}){
+function MessagePage({username, setUsername, userColor, setUserColor}){
+    const [userId, setUserId] = useState("")
+    const [userComment, setUserComment] = useState("No Comment")
+    const [timestamp, setTimestamp] = useState("1-21-2000")
+
     useEffect(() => {
         fetch("http://localhost:3000/users")
             .then((r) => r.json())
             .then((mdata) => mdata.map(messageData => {
-                return (
-                    <MessageCard 
-                        key = {messageData.id}
-                        username = {messageData.username}
-                        color = {messageData.color}
-                        comment = {messageData.comment}
-                        timestamp = {messageData.timestamp}
-                    />
-                )
+                setUsername(messageData.username)
+                setUserColor(messageData.color)
+                setUserId(messageData.id)
+                setUserComment(messageData.comment)
+                setTimestamp(messageData.timestamp)
             }))
     })
     return (
         <div>
             <div className = "messageBox">
                 <h1>Welcome to the Ozpod</h1>
-                <MessageCard />
+                <MessageCard
+                    key = {userId} 
+                    username = {username}
+                    color={userColor}
+                    timestamp={timestamp}
+                    userComment={userComment}/>
             </div>
             <MessageForm 
                 username = {username}
