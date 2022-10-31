a9a832e5ec9341eccdb563addef94128f1686ab9
Andrew Osborn <osbornandrewj@gmail.com>
2022-10-31 15:31:05 -0700

    got messages working

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 import MessageCard from "./MessageCard"
 
-function Messages(){    
+function Messages(){
+    const [mesArr, setMesArr] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:3000/posts")
+            .then((r) => r.json())
+            .then((mdata) => setMesArr([...mdata]))
+            // .then((mdata) => setMesArr([...mdata]))
+            }, [])
+    const posts = mesArr.map(message => {
+        return (
+            <MessageCard
+                key = {message.id}
+                username = {message.username}
+                messageColor = {message.color}
+                timestamp = {message.timestamp}
+                message = {message.message} />
+        )
+    })
+    console.log(posts)  
     return (
     <div>
-       <p>Placeholder</p>
+       {posts}
     </div>)   
 }
 
