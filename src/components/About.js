e33e139ac7d5a54740888bccba6e7994e4ccf905
Andrew Osborn <osbornandrewj@gmail.com>
2022-11-01 18:33:59 -0700

    project 'complete'

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 function About(){
     return (
-    <div>
-        <h1>Welcome to the About Section!</h1>
-        <ul>
-            <li>Logo from Dika Budi, at https://www.vecteezy.com/vector-art/5484075-orca-whale-logo</li>
-            <li><a href="https://www.freepik.com/free-photo/background-sea-water_4433046.htm#query=sea%20water&position=7&from_view=keyword">Image by kdekiara</a> on Freepik</li>
-        </ul>
+    <div id="about-section">
+        <h1 id="about-intro">Welcome to the About Section!</h1>
+        <p id="about-how-to">To use this webpage first choose a username and color on the login page, 
+            then click the login button to load onto the message board proper.
+            Once on the message board simply type your desired message and click send to chat
+            to your hearts content</p>
+        <p id="creator-contact">Contact the creator at osbornandrewj+OzpodMessenger@gmail.com</p>
+            <p className="sources">Logo from Dika Budi, at https://www.vecteezy.com/vector-art/5484075-orca-whale-logo</p>
+            <p className="sources"><a href="https://www.freepik.com/free-photo/background-sea-water_4433046.htm#query=sea%20water&position=7&from_view=keyword">Background Image by kdekiara</a> on Freepik</p>
     </div>
     )
 }
