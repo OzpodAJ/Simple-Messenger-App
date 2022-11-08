3d48968817f6498d759ed4d7b4183869d165d9de
Andrew Osborn <osbornandrewj@gmail.com>
2022-11-08 10:46:56 -0800

    fixed flaw with 'About' links

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ function About(){
             Once on the message board simply type your desired message and click send to chat
             to your hearts content</p>
         <p id="creator-contact">Contact the creator at osbornandrewj+OzpodMessenger@gmail.com</p>
-            <p className="sources">Logo from Dika Budi, at https://www.vecteezy.com/vector-art/5484075-orca-whale-logo</p>
-            <p className="sources"><a href="https://www.freepik.com/free-photo/background-sea-water_4433046.htm#query=sea%20water&position=7&from_view=keyword">Background Image by kdekiara</a> on Freepik</p>
+            <p className="sources"><a href="https://www.vecteezy.com/vector-art/5484075-orca-whale-logo" className="link">Logo from Dika Budi</a> on vecteezy</p>
+            <p className="sources"><a href="https://www.freepik.com/free-photo/background-sea-water_4433046.htm#query=sea%20water&position=7&from_view=keyword" className="link">Background Image by kdekiara</a> on Freepik</p>
     </div>
     )
 }
